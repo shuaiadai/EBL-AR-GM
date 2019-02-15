232cb1583a7b56bdaf8b97da59034b0577c299e4
MattiaZ85 <47643701+MattiaZ85@users.noreply.github.com>
2019-02-15 07:56:46 +0100

    Update exampleS.m

--- exampleS.m
+++ exampleS.m
@@ -1,1 +1,18 @@
 
+%% Install mex
+mex projectSortC.c
+
+%% generate the dataset (channels are independent)
+
+m = 5; % number of nodes
+N = 1000; % sample size 
+e = randn(N,m);
+y = zeros(N,m);
+for k = 3:N
+    y(k,:) = -0.1*y(k-1,:)-0.6*y(k-2,:)+e(k,:);
+end
+
+%% Indentification of a Sparse Graphical Model of order 2
+
+[Omega,S] = identS(y,2);
+
