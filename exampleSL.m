fa63c787a02613b72a2e30ec621f2af4aa4d34e3
MattiaZ85 <47643701+MattiaZ85@users.noreply.github.com>
2019-02-15 07:57:15 +0100

    Update exampleSL.m

--- exampleSL.m
+++ exampleSL.m
@@ -1,1 +1,17 @@
 
+%% Install mex
+mex projectSortC.c
+
+%% generate the dataset (manifest variables are conditionally independent, #latent variables=2)
+
+m = 5; % number of nodes
+N = 1000; % sample size 
+e = randn(N,m);
+y = zeros(N,m);
+for k=3:N
+    y(k,:) = -0.1*y(k-1,:)-0.6*y(k-2,:)+e(k,:)+0.3*randn(1,2)*ones(2,m);
+end
+
+%% Indentification of a Latent-variable Graphical Model of order 2
+
+[Omega,h,S,L] = identSL(y,2);
